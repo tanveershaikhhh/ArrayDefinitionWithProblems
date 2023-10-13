30caa1dc63fe823a4a4ba4fff275f6db42a621fe
tanveershaikhhh <102403198+tanveershaikhhh@users.noreply.github.com>
2023-10-13 23:40:28 +0530

    SubArray.java

--- SubArray.java
+++ SubArray.java
@@ -7,12 +7,15 @@ public class SubArray {
     public static void main(String[] args) {
        
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int arr [] = new int[n];
-        
+        int n = sc.nextInt();  // size of array
+        int arr [] = new int[n]; 
+
+        //Input the elements of Array 
         for (int i =0; i<n; i++){
             arr[i] = sc.nextInt();
         }
+
+        // intializing sum and count for counting 
         int sum=0;
         int count = 0;
         
