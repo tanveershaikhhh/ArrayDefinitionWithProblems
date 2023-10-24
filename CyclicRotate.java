aef827da8a52d7bc3a4d77f5adbffc0508b9a76e
Tanveer Shaikh <102403198+tanveershaikhhh@users.noreply.github.com>
2023-10-24 22:43:58 +0530

    CyclicRotate.java

--- CyclicRotate.java
+++ CyclicRotate.java
@@ -1,17 +1,16 @@
 public class CyclicRotate {
-    public static void main(String[] args) {
-        int arr[] = {10,20,30,40,50};
+   public static void main(String[] args) {
 
-        int end =arr[arr.length - 1];
+        int arr[] = {10, 20, 30, 40, 50};
+        int end = arr[arr.length - 1];
 
-        for (int i =1 ; i>arr.length; i++){
+        for (int i=arr.length-1; i>0; i--){
             arr[i] = arr[i-1];
         }
-
-        arr[0] = end;
-
         for (int i = 0; i < arr.length; i++) {
+            arr[0] = end;
             System.out.println(arr[i]);
+
         }
     }
 }
