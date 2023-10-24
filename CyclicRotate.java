cdb3e9af4f4fe67c067329e0be20eb1821ef73c9
Tanveer Shaikh <102403198+tanveershaikhhh@users.noreply.github.com>
2023-10-24 22:43:03 +0530

    Add files via upload

---
+++ CyclicRotate.java
@@ -0,0 +1,17 @@
+public class CyclicRotate {
+    public static void main(String[] args) {
+        int arr[] = {10,20,30,40,50};
+
+        int end =arr[arr.length - 1];
+
+        for (int i =1 ; i>arr.length; i++){
+            arr[i] = arr[i-1];
+        }
+
+        arr[0] = end;
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
